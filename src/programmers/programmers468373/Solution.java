62eb68db26d40901e2f6b960e1c6324fa48b3c95
dong99u <qkrehdrb0813@gmail.com>
2026-05-04 13:53:53 +0900

    문제

--- src/programmers/programmers468373/Solution.java
+++ src/programmers/programmers468373/Solution.java
@@ -1,4 +1,47 @@
 package programmers.programmers468373;
 
+import java.util.*;
+
 public class Solution {
+
+	public int solution(int n, int infection, int[][] edges, int k) {
+		List<List<int[]>> graph = initGraph(n, edges);
+		boolean[] visited = new boolean[n + 1];
+		List<Integer> infectedNodes = new ArrayList<>();
+		infectedNodes.add(infection);
+
+
+
+
+    }
+
+	static int bfs(List<Integer> startNodes, boolean[] visited) {
+		Queue<Integer> queue = new ArrayDeque<>(startNodes);
+
+		while (!queue.isEmpty()) {
+			int currNode = queue.poll();
+
+		}
+	}
+
+
+
+	static List<List<int[]>> initGraph(int n, int[][] edges) {
+		List<List<int[]>> graph = new ArrayList<>();
+
+		for (int i = 0; i <= n; i++) {
+			graph.add(new ArrayList<>());
+		}
+
+		for (int[] edge : edges) {
+			int from = edge[0];
+			int to = edge[1];
+			int category = edge[2];
+
+			graph.get(from).add(new int[] {to, category});
+			graph.get(to).add(new int[] {from, category});
+		}
+
+		return graph;
+	}
 }
--- src/swea/swea1855/Solution.java
+++ src/swea/swea1855/Solution.java
@@ -1,4 +1,69 @@
 package swea.swea1855;
 
+import java.util.*;
+import java.io.*;
+import java.util.stream.*;
+
 public class Solution {
+	static int n;
+	static int[] arr;
+	static BufferedReader br;
+
+	public static void main(String[] args) throws IOException {
+		br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine());
+
+		StringBuilder sb = new StringBuilder();
+		for (int tc = 1; tc <= t; tc++) {
+			init();
+			List<List<Integer>> graph = new ArrayList<>();
+			for (int i = 0; i <= n; i++) {
+				graph.add(new ArrayList<>());
+			}
+
+			for (int i = 2; i <= n ; i++) {
+				int u = i, v = arr[i - 2];
+				graph.get(u).add(v);
+				graph.get(v).add(u);
+			}
+			int answer = bfs(graph);
+			sb.append("#").append(tc).append(" ").append(answer).append("\n");
+
+		}
+
+		System.out.println(sb);
+	}
+
+	static int bfs(List<List<Integer>> graph) {
+		boolean[] visited = new boolean[n + 1];
+		Queue<int[]> queue = new ArrayDeque<>(1);
+		queue.add(new int[] {1, 0});
+		visited[1] = true;
+
+		int result = 0;
+		while (!queue.isEmpty()) {
+			int[] curr = queue.poll();
+			int currNode = curr[0], currDepth = curr[1];
+			if (queue.isEmpty())
+				result += currDepth;
+			else
+				result += currDepth * 2;
+			for (Integer next : graph.get(currNode)) {
+				if (!visited[next]) {
+					queue.add(new int[] {next, currDepth + 1});
+					visited[next] = true;
+				}
+			}
+		}
+
+		return result;
+	}
+
+	static void init() throws IOException {
+		n = Integer.parseInt(br.readLine());
+		arr = Arrays.stream(br.readLine().split(" "))
+			.mapToInt(Integer::parseInt)
+			.toArray();
+	}
+
 }
